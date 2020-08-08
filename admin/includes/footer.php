138a86390c8a3885b47f6cea6afb19e1d767bdff
Mubeen Afzal <mubeenafzal3@gmail.com>
2020-08-08 17:58:42 +0500

    sup

---
+++ admin/includes/footer.php
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2017 Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : PHPGURUKUL</a> 
+                </div>
+
+            </div>
+        </div>
+=======
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2017 Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : PHPGURUKUL</a> 
+                </div>
+
+            </div>
+        </div>
+>>>>>>> 91aa7545d4d6c353eb92e270e738cdb9ddd60bd3
+    </section>
---
+++ assets/js/custom.js
@@ -0,0 +1,103 @@
+<<<<<<< HEAD
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        slide_fun: function () {
+
+            $('#carousel-example').carousel({
+                interval:3000 // THIS TIME IS IN MILLI SECONDS
+            })
+
+        },
+        dataTable_fun: function () {
+
+            $('#dataTables-example').dataTable();
+
+        },
+       
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.slide_fun();
+        mainApp.dataTable_fun();
+        mainApp.custom_fun();
+    });
+}(jQuery));
+
+
+=======
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        slide_fun: function () {
+
+            $('#carousel-example').carousel({
+                interval:3000 // THIS TIME IS IN MILLI SECONDS
+            })
+
+        },
+        dataTable_fun: function () {
+
+            $('#dataTables-example').dataTable();
+
+        },
+       
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.slide_fun();
+        mainApp.dataTable_fun();
+        mainApp.custom_fun();
+    });
+}(jQuery));
+
+
+>>>>>>> 91aa7545d4d6c353eb92e270e738cdb9ddd60bd3
